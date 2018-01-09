5d7ed3fc6bafa169e439f9c4e08dc042e4d8e522
acbrent25 <adamchampagne@gmail.com>
2018-01-09 10:14:38 -0500

    added mailgun and password reset

--- routes/api/authentication.js
+++ routes/api/authentication.js
@@ -1,4 +1,10 @@
+const appConfig = require('../../config.js');
+const crypto = require('crypto');
 const express = require('express');
+const mailgun = require('mailgun-js')({
+  apiKey: appConfig.mailgun.apiKey,
+  domain: appConfig.mailgun.domain,
+});
 const mongoose = require('mongoose');
 const passport = require('passport');
 const User = require('../../models/user.js');
@@ -7,7 +13,6 @@ const router = express.Router();
 
 // configure mongoose promises
 mongoose.Promise = global.Promise;
-
 
 // GET to /logout
 router.get('/logout', (req, res) => {
@@ -41,7 +46,6 @@ router.post('/login', async (req, res) => {
   });
 });
 
-
 // POST to /register
 router.post('/register', async (req, res) => {
   // First, check and make sure the email doesn't already exist
@@ -71,6 +75,51 @@ router.post('/register', async (req, res) => {
   }
   // return an error if all else fails
   return res.send(JSON.stringify({ error: 'There was an error registering the user' }));
+});
+
+// POST to saveresethash
+router.post('/saveresethash', async (req, res) => {
+  let result;
+  try {
+    // check and make sure the email exists
+    const query = User.findOne({ email: req.body.email });
+    const foundUser = await query.exec();
+
+    // If the user exists, save their password hash
+    const timeInMs = Date.now();
+    const hashString = `${req.body.email}${timeInMs}`;
+    const secret = appConfig.crypto.secret;
+    const hash = crypto.createHmac('sha256', secret)
+                       .update(hashString)
+                       .digest('hex');
+    foundUser.passwordReset = hash;
+
+    foundUser.save((err) => {
+      if (err) { result = res.send(JSON.stringify({ error: 'Something went wrong while attempting to reset your password. Please Try again' })); }
+
+      // Put together the email
+      const emailData = {
+        from: `CloseBrace <postmaster@${appConfig.mailgun.domain}>`,
+        to: foundUser.email,
+        subject: 'Reset Your Password',
+        text: `A password reset has been requested for the MusicList account connected to this email address. If you made this request, please click the following link: https://musiclist.com/account/change-password/${foundUser.passwordReset} ... if you didn't make this request, feel free to ignore it!`,
+        html: `<p>A password reset has been requested for the MusicList account connected to this email address. If you made this request, please click the following link: <a href="https://musiclist.com/account/change-password/${foundUser.passwordReset}" target="_blank">https://musiclist.com/account/change-password/${foundUser.passwordReset}</a>.</p><p>If you didn't make this request, feel free to ignore it!</p>`,
+      };
+
+      // Send it
+      mailgun.messages().send(emailData, (error, body) => {
+        if (error || !body) {
+          result = res.send(JSON.stringify({ error: 'Something went wrong while attempting to send the email. Please try again.' }));
+        } else {
+          result = res.send(JSON.stringify({ success: true }));
+        }
+      });
+    });
+  } catch (err) {
+    // if the user doesn't exist, error out
+    result = res.send(JSON.stringify({ error: 'Something went wrong while attempting to reset your password. Please Try again' }));
+  }
+  return result;
 });
 
 module.exports = router;
--- src/actions/authentication.js
+++ src/actions/authentication.js
@@ -6,14 +6,16 @@ import { clearError } from './error';
 export const loginAttempt = () => ({ type: 'AUTHENTICATION_LOGIN_ATTEMPT' });
 export const loginFailure = error => ({ type: 'AUTHENTICATION_LOGIN_FAILURE', error });
 export const loginSuccess = json => ({ type: 'AUTHENTICATION_LOGIN_SUCCESS', json });
-export const sessionCheckFailure = () => ({ type: 'AUTHENTICATION_SESSION_CHECK_FAILURE' });
-export const sessionCheckSuccess = json => ({ type: 'AUTHENTICATION_SESSION_CHECK_SUCCESS', json });
-export const logoutFailure = error => ({ type: 'AUTHENTICATION_LOGOUT_FAILURE', error });
 export const logoutSuccess = () => ({ type: 'AUTHENTICATION_LOGOUT_SUCCESS' });
-export const registrationSuccessViewed = () => ({ type: 'AUTHENTICATION_REGISTRATION_SUCCESS_VIEWED' });
+export const logoutFailure = error => ({ type: 'AUTHENTICATION_LOGOUT_FAILURE', error });
 export const registrationFailure = error => ({ type: 'AUTHENTICATION_REGISTRATION_FAILURE', error });
 export const registrationSuccess = () => ({ type: 'AUTHENTICATION_REGISTRATION_SUCCESS' });
-
+export const registrationSuccessViewed = () => ({ type: 'AUTHENTICATION_REGISTRATION_SUCCESS_VIEWED' });
+export const passwordResetClear = () => ({ type: 'AUTHENTICATION_PASSWORD_RESET_CLEAR' });
+export const passwordResetHashCreated = () => ({ type: 'AUTHENTICATION_PASSWORD_RESET_HASH_CREATED' });
+export const passwordResetHashFailure = error => ({ type: 'AUTHENTICATION_PASSWORD_RESET_HASH_FAILURE', error });
+export const sessionCheckFailure = () => ({ type: 'AUTHENTICATION_SESSION_CHECK_FAILURE' });
+export const sessionCheckSuccess = json => ({ type: 'AUTHENTICATION_SESSION_CHECK_SUCCESS', json });
 
 // Check User Session
 export function checkSession() {
@@ -44,6 +46,45 @@ export function checkSession() {
   };
 }
 
+// Send email to API for hashing
+export function createHash(email) {
+  return async (dispatch) => {
+    // clear the error box if it's displayed
+    dispatch(clearError());
+
+    // turn on spinner
+    dispatch(incrementProgress());
+    // contact the API
+    await fetch(
+      // where to contact
+      '/api/authentication/saveresethash',
+      // what to send
+      {
+        method: 'POST',
+        body: JSON.stringify({ email }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'same-origin',
+      },
+    )
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        }
+        return null;
+      })
+      .then((json) => {
+        if (json.success) {
+          return dispatch(passwordResetHashCreated(json));
+        }
+        return dispatch(passwordResetHashFailure(new Error('Something went wrong. Please try again.')));
+      })
+      .catch(error => dispatch(passwordResetHashFailure(error)));
+    // turn off spinner
+    return dispatch(decrementProgress());
+  };
+}
 
 // Log User In
 export function logUserIn(userData) {
